de59f7c006182cc63476ecb6007a99d38c8a3ec5
umangm0389-sketch <umangm0389@gmail.com>
2025-12-29 12:38:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -247,8 +247,16 @@ class script(object):
 
 ᴍᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b>{file_name}\n
-📤 ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ: <ahref="https://t.me/+y5H21xa2cg0Mzc1">Cinema Hub</a></b>"""
+    CAPTION = """<b>🎬 {file_name}
+
+━━━━━━━━━━━━━━━━━━
+📤 <u>UPLOADED BY</u> :
+🎥 <a href="https://t.me/+y5H21xa2cg0Mzc1">Cinema Hub</a>
+
+━━━━━━━━━━━━━━━━━━
+⚡ Stay Connected For Latest Movies & Series
+🔥 Fast • Clean • Reliable
+━━━━━━━━━━━━━━━━━━</b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🎬 ᴛɪᴛʟᴇ: <a href={url}>{title}</a>
