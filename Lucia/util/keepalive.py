46fd053f95ceb6d056a4b0b7143468cb63b07e95
umangm0389-sketch <umangm0389@gmail.com>
2025-12-31 08:47:13 +0530

    Update keepalive.py

--- Lucia/util/keepalive.py
+++ Lucia/util/keepalive.py
@@ -1,21 +1,2 @@
-import asyncio
-import aiohttp
-import traceback
-from info import *
-from logging_helper import LOGGER
-
-
-async def ping_server():
-    sleep_time = PING_INTERVAL
-    while True:
-        await asyncio.sleep(sleep_time)
-        try:
-            async with aiohttp.ClientSession(
-                timeout=aiohttp.ClientTimeout(total=10)
-            ) as session:
-                async with session.get(URL) as resp:
-                    LOGGER.info("Pinged server with response: {}".format(resp.status))
-        except TimeoutError:
-            LOGGER.error("Couldn't connect to the site URL..!")
-        except Exception:
-            traceback.print_exc()
+# keepalive disabled for Render free hosting
+# Render does not allow internal ping on 0.0.0.0
